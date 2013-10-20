ca393881f539e428ee57187d5c64794603b56f04
Tomas Perez <tom@0x101.com>
2013-10-20 22:30:11 +0400

    Moving the event emitter functionality to new object.

--- src/js/web-terminal.js
+++ src/js/web-terminal.js
@@ -1,7 +1,7 @@
 /**
  * @object WebTerminal
  */
-var WebTerminal = (function() {
+var WebTerminal = (function(EventEmitter) {
 
   /**
    * @constructor
@@ -20,25 +20,11 @@ var WebTerminal = (function() {
       }
     };
 
-    this._listeners = {};
+    EventEmitter.apply(this);
 
   };
 
-  /**
-   * @param {String} eventId
-   * @param {Function} callback
-   * @method attach
-   * @public
-   */
-  WebTerminal.prototype.attach = function(eventId, callback) {
-
-    if (typeof this._listeners[eventId] === 'undefined') {
-      this._listeners[eventId] = [];
-    }
-
-    this._listeners[eventId].push(callback);
-
-  };
+  WebTerminal.prototype = new EventEmitter();
 
   /**
    * Preload templates via AJAX request, from the resources folder.
@@ -59,32 +45,12 @@ var WebTerminal = (function() {
           templates[key] = content;
 
           if (pendingResources === 0) {
-            self._emit('ready');
+            self.emit('ready');
           }
 
         });
       }
     }
-  };
-
-  /**
-   * @param {String} eventId
-   * @param {undefined|Object} data 
-   * @method _emit
-   * @private
-   */
-  WebTerminal.prototype._emit = function(eventId, data) {
-
-    if (typeof this._listeners[eventId] !== 'undefined') {
-
-      for(var i = 0; i < this._listeners[eventId].length; i++) {
-        if (typeof this._listeners[eventId][i] === 'function') {
-          this._listeners[eventId][i](data);
-        }
-      }
-
-    }
-
   };
 
   /**
@@ -94,7 +60,7 @@ var WebTerminal = (function() {
   WebTerminal.prototype._addLine = function() {
 
     var content = Mustache.render(this.config.templates['line'], {
-      'context': 'asdf'
+      'context': ''
     });
 
     var node = document.createElement('div');
@@ -135,4 +101,4 @@ var WebTerminal = (function() {
 
   return WebTerminal;
 
-})();
+})(EventEmitter);
